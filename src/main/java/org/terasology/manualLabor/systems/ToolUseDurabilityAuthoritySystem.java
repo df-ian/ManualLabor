d7a28c32c0ce9fe61119b0a725604b60d6d6903a
Josh Zacharias <josh.owie@gmail.com>
2014-12-09 08:10:42 -0800

    Use the process entity's input items to determine how much durability is used.

--- src/main/java/org/terasology/manualLabor/systems/ToolUseDurabilityAuthoritySystem.java
+++ src/main/java/org/terasology/manualLabor/systems/ToolUseDurabilityAuthoritySystem.java
@@ -22,11 +22,21 @@ import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.machines.events.RequirementUsedEvent;
+import org.terasology.workstation.process.inventory.InventoryInputProcessPartSlotAmountsComponent;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class ToolUseDurabilityAuthoritySystem extends BaseComponentSystem {
     @ReceiveEvent
     public void onToolUse(RequirementUsedEvent event, EntityRef entity) {
-        entity.send(new ReduceDurabilityEvent(1));
+        InventoryInputProcessPartSlotAmountsComponent slotAmounts = event.getProcessEntity().getComponent(InventoryInputProcessPartSlotAmountsComponent.class);
+        if (slotAmounts != null) {
+            int totalAmount = 0;
+            for (Integer itemAmount : slotAmounts.slotAmounts.values()) {
+                totalAmount += itemAmount;
+            }
+            entity.send(new ReduceDurabilityEvent(totalAmount));
+        } else {
+            entity.send(new ReduceDurabilityEvent(1));
+        }
     }
 }
