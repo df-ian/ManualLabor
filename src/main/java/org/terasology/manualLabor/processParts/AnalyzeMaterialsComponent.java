393f8945a602cd7c6877a8e0f46aa3640f684984
Josh Zacharias <josh.owie@gmail.com>
2015-01-26 14:31:27 -0800

    Campfire casing fix.  Add dirty glass made from normal sand.  Add material analysis with the crafted magnifying glass.  This is a precurser to #2.

---
+++ src/main/java/org/terasology/manualLabor/processParts/AnalyzeMaterialsComponent.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.manualLabor.processParts;
+
+import org.terasology.entitySystem.Component;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.logic.inventory.InventoryManager;
+import org.terasology.manualLabor.components.AnalyzedMaterialComponent;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.substanceMatters.components.MaterialCompositionComponent;
+import org.terasology.workstation.process.ProcessPart;
+import org.terasology.workstation.process.WorkstationInventoryUtils;
+import org.terasology.workstation.process.inventory.ValidateInventoryItem;
+
+public class AnalyzeMaterialsComponent implements Component, ProcessPart, ValidateInventoryItem {
+
+    @Override
+    public boolean isResponsibleForSlot(EntityRef workstation, int slotNo) {
+        return WorkstationInventoryUtils.getAssignedSlots(workstation, "INPUT").contains(slotNo)
+                || WorkstationInventoryUtils.getAssignedSlots(workstation, "OUTPUT").contains(slotNo);
+    }
+
+    @Override
+    public boolean isValid(EntityRef workstation, int slotNo, EntityRef instigator, EntityRef item) {
+        if (WorkstationInventoryUtils.getAssignedSlots(workstation, "INPUT").contains(slotNo)) {
+            return item.hasComponent(MaterialCompositionComponent.class) && !item.hasComponent(AnalyzedMaterialComponent.class);
+        } else if (WorkstationInventoryUtils.getAssignedSlots(workstation, "OUTPUT").contains(slotNo)) {
+            return item.hasComponent(AnalyzedMaterialComponent.class);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean validateBeforeStart(EntityRef instigator, EntityRef workstation, EntityRef processEntity) {
+        InventoryManager inventoryManager = CoreRegistry.get(InventoryManager.class);
+        for (int inputSlot : WorkstationInventoryUtils.getAssignedSlots(workstation, "INPUT")) {
+            EntityRef inputItem = inventoryManager.getItemInSlot(workstation, inputSlot);
+            if (inputItem.exists()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public long getDuration(EntityRef instigator, EntityRef workstation, EntityRef processEntity) {
+        return 0;
+    }
+
+    @Override
+    public void executeStart(EntityRef instigator, EntityRef workstation, EntityRef processEntity) {
+
+    }
+
+    @Override
+    public void executeEnd(EntityRef instigator, EntityRef workstation, EntityRef processEntity) {
+        InventoryManager inventoryManager = CoreRegistry.get(InventoryManager.class);
+
+        for (int inputSlot : WorkstationInventoryUtils.getAssignedSlots(workstation, "INPUT")) {
+            EntityRef inputItem = inventoryManager.getItemInSlot(workstation, inputSlot);
+            if (inputItem.exists()) {
+                if (!inputItem.hasComponent(AnalyzedMaterialComponent.class)) {
+                    inputItem.addComponent(new AnalyzedMaterialComponent());
+                }
+                inventoryManager.moveItemToSlots(instigator, workstation, inputSlot, workstation, WorkstationInventoryUtils.getAssignedSlots(workstation, "OUTPUT"));
+            }
+        }
+    }
+}
