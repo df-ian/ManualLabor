7dbf3d345b035497b69d1a2842e1de0c36b15808
DarkWeird <foxflameless@gmail.com>
2020-08-24 16:06:55 +0300

    fix: NUI extraction compability

--- src/main/java/org/terasology/manualLabor/processParts/HeatInputProcessPartCommonSystem.java
+++ src/main/java/org/terasology/manualLabor/processParts/HeatInputProcessPartCommonSystem.java
@@ -1,34 +1,17 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.manualLabor.processParts;
 
-import org.terasology.entityNetwork.Network;
-import org.terasology.entityNetwork.NetworkNode;
 import org.terasology.entityNetwork.systems.EntityNetworkManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.machines.ui.OverlapLayout;
-import org.terasology.manualLabor.components.HeatBlockNetworkComponent;
-import org.terasology.manualLabor.components.HeatSourceComponent;
 import org.terasology.manualLabor.components.HeatedComponent;
 import org.terasology.manualLabor.systems.HeatAuthoritySystem;
+import org.terasology.nui.widgets.UIImage;
 import org.terasology.registry.In;
-import org.terasology.rendering.nui.widgets.UIImage;
 import org.terasology.utilities.Assets;
 import org.terasology.workstation.process.ProcessPartDescription;
 import org.terasology.workstation.processPart.ProcessEntityFinishExecutionEvent;
--- src/main/java/org/terasology/manualLabor/processParts/ProcessingTimeProcessPartCommonSystem.java
+++ src/main/java/org/terasology/manualLabor/processParts/ProcessingTimeProcessPartCommonSystem.java
@@ -1,33 +1,17 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.manualLabor.processParts;
 
-import com.google.common.collect.Lists;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.machines.ui.OverlapLayout;
-import org.terasology.rendering.nui.widgets.UIImage;
+import org.terasology.nui.widgets.UIImage;
 import org.terasology.utilities.Assets;
 import org.terasology.workstation.process.ProcessPartDescription;
 import org.terasology.workstation.processPart.ProcessEntityGetDurationEvent;
 import org.terasology.workstation.processPart.metadata.ProcessEntityGetInputDescriptionEvent;
-
-import java.util.List;
 
 @RegisterSystem
 public class ProcessingTimeProcessPartCommonSystem extends BaseComponentSystem {
@@ -40,7 +24,6 @@ public class ProcessingTimeProcessPartCommonSystem extends BaseComponentSystem {
     @ReceiveEvent
     public void getInputDescriptions(ProcessEntityGetInputDescriptionEvent event, EntityRef processEntity,
                                      ProcessingTimeComponent processingTimeComponent) {
-        List<ProcessPartDescription> descriptions = Lists.newLinkedList();
         String time = String.valueOf(processingTimeComponent.duration / 1000);
         UIImage image = new UIImage(Assets.getTextureRegion("ManualLabor:Manuallabor#Time").get());
         OverlapLayout layout = new OverlapLayout();
