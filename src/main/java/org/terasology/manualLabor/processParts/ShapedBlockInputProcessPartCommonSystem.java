dd43a5cc905c06fc784fcf543a6a8fd10a2f02f1
Michael Pollind <mpollind@gmail.com>
2021-04-03 08:53:10 -0700

    chore: migrate gestalt v7
     PR: https://github.com/MovingBlocks/Terasology/pull/4593

--- src/main/java/org/terasology/manualLabor/processParts/ShapedBlockInputProcessPartCommonSystem.java
+++ src/main/java/org/terasology/manualLabor/processParts/ShapedBlockInputProcessPartCommonSystem.java
@@ -21,7 +21,7 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.assets.ResourceUrn;
+import org.terasology.gestalt.assets.ResourceUrn;
 import org.terasology.engine.entitySystem.entity.EntityBuilder;
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
