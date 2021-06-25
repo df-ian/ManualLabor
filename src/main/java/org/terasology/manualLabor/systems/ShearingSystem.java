64e3cd12a79858b373f918398b53ed420d922622
meetcshah19 <meetcshah19@gmail.com>
2021-06-25 22:19:30 +0530

    refactor

--- src/main/java/org/terasology/manualLabor/systems/ShearingSystem.java
+++ src/main/java/org/terasology/manualLabor/systems/ShearingSystem.java
@@ -11,7 +11,6 @@ import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.prefab.Prefab;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
-import org.terasology.engine.logic.characters.CharacterHeldItemComponent;
 import org.terasology.engine.logic.characters.events.AttackEvent;
 import org.terasology.engine.logic.delay.DelayManager;
 import org.terasology.engine.logic.delay.PeriodicActionTriggeredEvent;
@@ -30,6 +29,10 @@ public class ShearingSystem extends BaseComponentSystem {
     public static final String SHEARING_ITEM = "ManualLabor:CrudeShears";
     public static final int HAIR_REGROWTH_TIME = 3 * 60 * 1000; // 3 minutes in ms
     public static final String HAIR_REGROWTH_ACTION_ID = "ManualLabor:HairRegrowthAction";
+    public static final String SHEARED_SHEEP_MESH = "WildAnimals:shearedSheep";
+    public static final String SHEARED_SHEEP_MATERIAL = "WildAnimals:shearedSheepSkin";
+    public static final String SHEEP_MESH = "WildAnimals:sheep";
+    public static final String SHEEP_MATERIAL = "WildAnimals:sheepSkin";
     private static final Logger logger = LoggerFactory.getLogger(ShearingSystem.class);
 
     @In
@@ -44,23 +47,14 @@ public class ShearingSystem extends BaseComponentSystem {
     @ReceiveEvent(components = {ShearableComponent.class})
     public void onAttack(AttackEvent event, EntityRef entityRef) {
         ShearableComponent component = entityRef.getComponent(ShearableComponent.class);
-        EntityRef player = event.getInstigator();
-        CharacterHeldItemComponent characterHeldItemComponent = player.getComponent(CharacterHeldItemComponent.class);
-        EntityRef heldItem = characterHeldItemComponent.selectedItem;
+        EntityRef heldItem = event.getDirectCause();
         Prefab parentPrefab = heldItem.getParentPrefab();
         if (parentPrefab != null) {
             if (!component.sheared && parentPrefab.getUrn().equals(new ResourceUrn(SHEARING_ITEM))) {
                 component.sheared = true;
                 component.lastShearingTimestamp = time.getGameTimeInMs();
                 delayManager.addPeriodicAction(entityRef, HAIR_REGROWTH_ACTION_ID, 0, HAIR_REGROWTH_TIME / 20);
-                SkeletalMeshComponent skeletalMeshComponent = entityRef.getComponent(SkeletalMeshComponent.class);
-                Optional<SkeletalMesh> skeletalMesh = assetManager.getAsset("WildAnimals:shearedSheep",
-                        SkeletalMesh.class);
-                skeletalMesh.ifPresent(mesh -> skeletalMeshComponent.mesh = mesh);
-                Optional<Material> shearedMaterial = assetManager.getAsset("WildAnimals:shearedSheepSkin",
-                        Material.class);
-                shearedMaterial.ifPresent(texture -> skeletalMeshComponent.material = shearedMaterial.get());
-                entityRef.saveComponent(skeletalMeshComponent);
+                switchPrfab(entityRef, SHEARED_SHEEP_MESH, SHEARED_SHEEP_MATERIAL);
             }
         }
     }
@@ -73,15 +67,26 @@ public class ShearingSystem extends BaseComponentSystem {
                 if ((time.getGameTimeInMs() - shearableComponent.lastShearingTimestamp) > HAIR_REGROWTH_TIME) {
                     shearableComponent.sheared = false;
                     delayManager.cancelPeriodicAction(entity, HAIR_REGROWTH_ACTION_ID);
-                    SkeletalMeshComponent skeletalMeshComponent = entity.getComponent(SkeletalMeshComponent.class);
-                    Optional<SkeletalMesh> skeletalMesh = assetManager.getAsset("WildAnimals:sheep",
-                            SkeletalMesh.class);
-                    skeletalMesh.ifPresent(mesh -> skeletalMeshComponent.mesh = mesh);
-                    Optional<Material> sheepMaterial = assetManager.getAsset("WildAnimals:sheepSkin", Material.class);
-                    sheepMaterial.ifPresent(texture -> skeletalMeshComponent.material = sheepMaterial.get());
-                    entity.saveComponent(skeletalMeshComponent);
+                    switchPrfab(entity, SHEEP_MESH, SHEEP_MATERIAL);
                 }
             }
         }
     }
+
+    private void switchPrfab(EntityRef entity, String meshURI, String materialURI) {
+        SkeletalMeshComponent skeletalMeshComponent = entity.getComponent(SkeletalMeshComponent.class);
+        if (skeletalMeshComponent != null) {
+            Optional<SkeletalMesh> skeletalMesh = assetManager.getAsset(meshURI,
+                    SkeletalMesh.class);
+            Optional<Material> shearedMaterial = assetManager.getAsset(materialURI,
+                    Material.class);
+            if (skeletalMesh.isPresent() && shearedMaterial.isPresent()) {
+                skeletalMeshComponent.mesh = skeletalMesh.get();
+                skeletalMeshComponent.material = shearedMaterial.get();
+            }
+            entity.saveComponent(skeletalMeshComponent);
+        }
+    }
+
 }
+
